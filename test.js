efe4bac7b24fb75b73fe16f035a1fad1e0786a5b
geteacup <lhsang23404@gmail.com>
2024-12-24 17:23:25 +0700

    đ

--- test.js
+++
@@ -1,8 +0,0 @@
-const connectToDB = require('./db'); // Đảm bảo file db.js có trong cùng thư mục
-
-async function testConnection() {
-    const db = await connectToDB();
-    console.log('Kết nối xong, database:', db.databaseName);
-}
-
-testConnection();
