02f9934c738a44c748db62da0bce39e0a491a823
Hello231104 <lh3399257@gmail.com>
2024-12-24 18:11:23 +0700

    alo

--- testdb.js
+++
@@ -1,8 +0,0 @@
-const connectToDB = require('db.js');
-
-async function testConnection() {
-    const db = await connectToDB();
-    console.log('Kết nối xong, database:', db.databaseName);
-}
-
-testConnection();
